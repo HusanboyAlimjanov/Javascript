d7d4247fdc4febab2e49cf377a4a2eb8d70c613e
HusanboyAlimjanov <brokerxm734@gmail.com>
2026-04-03 10:37:44 +0500

    git buyruqlari darsi

--- 1- BO'LIM/lesson6/index.js
+++ 1- BO'LIM/lesson6/index.js
@@ -1,1 +1,21 @@
 console.log("github")
+//git int
+//git remode add origin https://repozitory-manzil
+//git add
+//git comit -m "commentariya yoziladi"
+
+//git push origin main
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++==
+//qo'shimcha so'rab qolsa:
+//git config --global user.email"eshmatgmail.com"
+//git config -- global user.name"eshmat toshmatov"
+
+//doim kod yozsak 3 ta hgit buyrug'ini ishlatishimiz shart
+
+
+//git add
+//git commit -m "komentariya"
+//git push origin matn
