bab668033fc5326c45f0cfea36b03a8b78c68a88
HusanboyAlimjanov <brokerxm734@gmail.com>
2026-04-06 10:33:42 +0500

    foor loop

---
+++ 1- BO'LIM/lesson7/index.js
@@ -0,0 +1,28 @@
+//for loop
+//start = 1
+//stop = 10
+// for(let raqam = 1; raqam <=10; raqam++){
+//     console.log(raqam);
+// }
+
+// for (let raqam = 10; raqam >= 1; raqam--){
+//     console.log(raqam)
+// }
+
+// //=========================================================
+
+// for (let raqam1 = 10; raqam1 >= 1; raqam1 -=){
+//     console.log(raqam)
+// }
+
+// for (let raqam = 1; raqam <= 10; raqam++){
+//     if(raqam %2 ===1)
+//     console.log(raqam);
+    
+// }
+
+// for (let raqam = 1; raqam <= 10; raqam++){
+//     if(raqam %2 ===0)
+//     console.log(raqam);
+    
+// }
