ee19e3a5571f06467dcdfd9edf62ff636b6819e4
HusanboyAlimjanov <brokerxm734@gmail.com>
2026-04-10 09:49:03 +0500

    while and do while

--- 1- BO'LIM/lesson7/index.js
+++ 1- BO'LIM/lesson7/index.js
@@ -56,9 +56,7 @@
     
     
 // }
-// for (let i = 10; i >= 1; i--){
-    // console.log(i)
-// }
+
 // //=========================================================
 
 // let n = 10
@@ -107,11 +105,138 @@
 
 
 
-// //=========================================================
+//=========================================================
 // let n = 9;
 // for(let i = 1; i <= n; i++){
 //     for (let k = 1; k <= 10; k++){
-//         console.log('${i} x ${k} = ${i * k}');
+//         console.log(`${i} x ${k} = ${i * k}`);
 //     }
 //     console.log("===========================")
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//vazifa
+//1
+// for(let i =5; i <=15; i++){
+//     console.log(i)
+// }
+
+//2
+// let n = 1
+// for(let i = 1 ; i <= 200; i++){
+//     if(n += i)
+//         console.log(n)
+// }
+
+//3
+// // for (let raqam = 1; raqam <= 60; raqam++){
+//     if(raqam %2 ===0)
+//     console.log(raqam);
+    
+// }
+//4
+// for (let raqam = 1; raqam <= 60; raqam++){
+//     if(raqam %2 ===1)
+//     console.log(raqam);
+    
+// }
+//5
+// let n = 20
+// let k = n*n*n
+// for(let i = 1; i <= n; i++){
+//         console.log(k)
+// }
+
+//6
+// let n = 27
+// let count = 0
+// for(let i =1; i <= n; i++){
+//     if(i % 4 === 0) count ++
+// }
+
+// console.log(count);
+//7
+// let n = 200;
+// let k
+// for(let i = 1 ; i <= n; i++){
+//     kopaytma += i;
+// }
+// console.log(kopaytma)
+//8
+// for (let i = 1; i <= 150; i++){
+//     if(i % 3 === 0 && i% 5 === 0)
+//         console.log(i)
+    
+    
+// }
+//9
+// for (let i = 20; i >= 1; i--){
+//     console.log(i)
+// }
+//10
+// for (let raqam = 1; raqam <= 10; raqam++){
+//     if(raqam %2 ===0)
+//     console.log(raqam);
+    
+// }
+//11
+// let n = 20
+// for (let raqam = 1; raqam <= n; raqam--){
+//     if(raqam %2 === 0)
+//     console.log(raqam);
+    
+// }
+//12
+// let n = 100
+// for(let raqam = 1; raqam <= n; raqam ++){
+//     if(raqam < 2)console.log(raqam)
+// }
+//13
+// let n = 30
+// for(let i = 1; i <= n; i++){
+//     if(i % 3 === 0)
+//         console.log(i);   
+// }
+//14
+// let n = 30
+// for(let raqam = 1; raqam <= n; raqam ++){
+//     if(raqam < 20)console.log(raqam)
+// }
+
+
+//15
+// let n = 9;
+// for(let i = 1; i <= n; i++){
+//     for (let k = 1; k <= 10; k++){
+//         console.log(`${i} + ${k} = ${i + k}`);
+//     }
+//     console.log("===========================")
+// }
+//16
+// for (let raqam = 1; raqam <= 100; raqam++){
+//     if(raqam %9 ===0)
+//     console.log(raqam);
+    
+// }
+//18
+for (let raqam = 1; raqam <= 40; raqam++){
+    if(raqam %6 ===0 )
+    console.log(raqam);
+    
+}
--- 1- BO'LIM/lesson8/index.js
+++ 1- BO'LIM/lesson8/index.js
@@ -1,129 +1,107 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//while
+//do while
+
+//FOR LOOP
+// for(let raqam1 = 1; raqam1 <= 10; raqam1++){
+//     console.log(raqam1);
+// }
+
+//WHILE LOOP
+// let raqam2 = 1
+// while(raqam2 <= 10){
+//     console.log(raqam2)
+//     raqam2++
+// }
+
+//DOO WHILE LOOP
+// let raqam3 = 1
+// do{
+//     console.log(raqam3)
+//     raqam3++
+// }while(raqam3<= 10)
+
+
+
+//2
+// let raqam = 1
+// let yigindi = 0
+// while(raqam <= 100){
+//     yigindi += raqam;
+//     raqam++;
+// }
+// console.log(yigindi)
+//=========================
+
+// let raqam2 = 1
+// let yigindi2 = 0
+// do{
+//     yigindi2 += raqam2;
+//     raqam2++
+// }while(raqam2 <= 100)
+//     console.log(yigindi2)
+
+
+//3
+// let raqam1 = 1
+// while(raqam1 <= 50){
+//     if(raqam1 %2 === 0);console.log(raqam1)
+//     raqam1++;
+// }
+// let raqam2 = 1
+// do{
+//     if(raqam2 %2 === 0)console.log(raqam2)
+//         raqam2++
+// }while(raqam2 <= 50)
+
+
+//4
+// let raqam1 = 1
+// while(raqam1 <= 50){
+//     if(raqam1 %2 === 1);console.log(raqam1)
+//     raqam1++;
+// }
+// let raqam2 = 1
+// do{
+//     if(raqam2 %2 === 1)console.log(raqam2)
+//         raqam2++
+// }while(raqam2 <= 50)
+
+
+//5
+// let n = 15
+// let raqam1 = 1
+// while(raqam1 <= n){
+//     console.log(raqam1**2)
+//     raqam1++
+// }
+// let raqam2 =1
+
+// do{
+//     console.log(raqam2*raqam2)
+//     raqam2++
+// }while(raqam2 <= n)
+//6
+
+// let raqam1 = 1
+// let n = 20
+// while(raqam1 <= n){
+//     if(raqam1 %3 === 0)console.log(raqam1)
+//         raqam1++
+// }
+// raqam2 = 1
+// do{
+//     if(raqam2 %3 === 0)console.log(raqam2)
+//     raqam2++
+// }while(raqam2 <= n)
+
+//7
+// raqam1 = 1
+// n = 10
+// kopaytma = 1
+// while(raqam1 <= n){
+//     kopaytma *= raqam1 
+//     raqam1++
+// }
+// console.log(kopaytma)
 
 
